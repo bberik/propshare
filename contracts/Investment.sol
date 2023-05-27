3bffa0cbe10b82018ffe58cb03ad1598d0ac5f43
Berik Bazarbayev <berik@Beriks-MacBook-Air.local>
2023-05-27 21:58:38 +0900

    implemented listing and investment

---
+++ contracts/Investment.sol
@@ -0,0 +1,100 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(address _from, address _to, uint256 _id) external;
+}
+
+contract Investment {
+    address public nftAddress;
+    address payable public owner;
+    address public inspector;
+
+    modifier onlyInspector() {
+        require(msg.sender == inspector, "Only inspector can call this method");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this method");
+        _;
+    }
+
+    mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => uint256) public numberOfTotalTokens;
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public totalPrice;
+    mapping(uint256 => mapping(address => uint256)) public investors;
+    mapping(uint256 => uint256) public tokenPrice;
+    mapping(uint256 => uint256) numberOfAvailableTokens;
+
+    constructor(
+        address _nftAddress,
+        address payable _owner,
+        address _inspector
+    ) {
+        nftAddress = _nftAddress;
+        owner = _owner;
+        inspector = _inspector;
+    }
+
+    function list(
+        uint256 _nftID,
+        uint256 _totalPrice,
+        uint256 _sharedPersentage
+    ) public payable onlyOwner {
+        require(
+            _sharedPersentage <= 75 && _sharedPersentage >= 20,
+            "Owner should keep at least 25% of the property, while offering at least 20% of property shares"
+        ); // hardcoded
+
+        require(
+            _totalPrice > 0,
+            "Total value of the property should be greater than 0"
+        );
+
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        totalPrice[_nftID] = _totalPrice;
+        numberOfTotalTokens[_nftID] = _sharedPersentage * 100; //hardcoded number of tokens (have to make it either input or global variable)
+        numberOfAvailableTokens[_nftID] = numberOfTotalTokens[_nftID];
+        tokenPrice[_nftID] = _totalPrice / 10000; // hardcoded (each token is 0.01% of the property value)
+    }
+
+    function invest(
+        uint256 _nftID,
+        uint256 _numberOfTokensInvested
+    ) public payable {
+        require(
+            inspectionPassed[_nftID] == true,
+            "The property didn't pass the inspection check yet, cannot be invested"
+        );
+        require(
+            _numberOfTokensInvested > 0 &&
+                _numberOfTokensInvested <= numberOfAvailableTokens[_nftID],
+            "Number of tokens should be greater than zero and less than number of available tokens"
+        );
+        require(
+            msg.value == _numberOfTokensInvested * tokenPrice[_nftID],
+            "Not exact amount of Ether transfered to purchase tokens"
+        );
+        investors[_nftID][msg.sender] = _numberOfTokensInvested;
+        numberOfAvailableTokens[_nftID] -= _numberOfTokensInvested;
+
+        // To-do: Get the ERC20 tokens from Tokens.sol contract and send it to investors wallet
+    }
+
+    function updateInspectionStatus(
+        uint256 _nftID,
+        bool _passed
+    ) public onlyInspector {
+        inspectionPassed[_nftID] = _passed;
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,102 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let owner, investors, inspector
+    let realEstate, investment
+    
+    beforeEach(async () => {
+        [owner, inspector, ...investors] = await ethers.getSigners()
+    
+            const RealEstate = await ethers.getContractFactory('RealEstate')
+            realEstate = await RealEstate.deploy()
+        
+            let transaction = await realEstate.connect(owner).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+            await transaction.wait()
+    
+            const Investment = await ethers.getContractFactory('Investment')
+            investment = await Investment.deploy(realEstate.address, owner.address, inspector.address)
 
+            transaction = await realEstate.connect(owner).approve(investment.address, 1)
+            await transaction.wait()
+
+            transaction = await investment.connect(owner).list(1, tokens(10), 70)
+            await transaction.wait()
+
+
+    })
+
+    describe('Deployment', () => {
+        it('Returns NFT address', async () => {
+            const result = await investment.nftAddress()
+            expect(result).to.be.equal(realEstate.address)
+        })
+
+        it('Returns Owner address', async () => {
+            const result = await investment.owner()
+            expect(result).to.be.equal(owner.address)
+        })
+
+        it('Returns Inspector address', async () => {
+            const result = await investment.inspector()
+            expect(result).to.be.equal(inspector.address)
+        })
+
+
+    })
+
+    describe('Listing', () => {
+        it('Updates as listed', async () => {
+            const result = await investment.isListed(1)
+            expect(result).to.be.equal(true)
+        })
+
+        it('Returns number of total tokens', async () => {
+            const result = await investment.numberOfTotalTokens(1)
+            console.log(result)
+            // expect(result).to.be.equal()
+        })
+
+        it('Returns total price', async () => {
+            const result = await investment.totalPrice(1)
+            console.log(result)
+            // expect(result).to.be.equal()
+        })
+
+        it('Returns token price', async () => {
+            const result = await investment.tokenPrice(1)
+            console.log(result)
+            // expect(result).to.be.equal()
+        })
+
+        it('Updates ownership', async () => {
+            expect(await realEstate.ownerOf(1)).to.be.equal(investment.address)
+        })
+    })
+
+    describe('Investment', () => {
+        beforeEach(async () => {
+            let transaction = await investment.connect(inspector).updateInspectionStatus(1, true)
+            await transaction.wait()
+
+            transaction = await investment.connect(investors[0]).invest(1, 1000, { value: tokens(1) })
+                await transaction.wait()
+            
+            transaction = await investment.connect(investors[1]).invest(1, 2000, { value: tokens(2) })
+                await transaction.wait()
+            
+        })
+        
+        it('List of investors', async () => {
+            const result = await investment.investors(1, investors[1].address)
+            console.log(result)
+        })
+
+        it('Updates contract balance', async () => {
+            const result = await investment.getBalance()
+            expect(result).to.be.equal(tokens(3))
+        })
+    })
+    
+    
+    
 })
